15a100d956960b805a00452872227c8c7641b5b3
NIHIL-COGITOLEX <arsh9321021942@gmail.com>
2026-05-06 04:18:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,8 @@ app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
-app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_DEFAULT_SENDER"] = "hr@alfatza.com"
+app.config["MAIL_TIMEOUT"] = 20
 
 mail = Mail(app)
 
